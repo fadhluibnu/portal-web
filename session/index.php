25f898e48d5e2ce0cedee6a2ca8da0c59bf1ff02
dimitri23559 <63942553+dimitri23559@users.noreply.github.com>
2021-10-26 21:10:13 +0700

    eror

--- session/index.php
+++ session/index.php
@@ -1,3 +1,20 @@
+<?php
+require 'function.php';
+
+if (isset($_POST["register"])) {
+    if(registrasi($_POST) > 0 ){
+        echo "<script>
+            alert('user baru berhasil ditambahkan');
+             <script>";
+
+    
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
+
+?>
 <!doctype html>
 <html lang="en">
 
