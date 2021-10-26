5cea01429ee57fcd26ae2a7e33ff561861a6a156
fadhluibnu <f.ibnu.a1@gmail.com>
2021-10-26 21:28:19 +0700

    update funtion

--- function.php
+++ function.php
@@ -1,13 +1,13 @@
 <?php
 
 //koneksi
-$conn = mysqli_connect ("localhost","root","portal-dagang");
+$conn = mysqli_connect("localhost", "root", "", "portal-dagang");
 
-function registrasi($data){
+function registrasi($data)
+{
     global $conn;
 
     $username = strtolower(stripslashes($data["username"]));
-    $password = mysqli_real_escape_string($conn,$data["password"]);
-    $password2 = mysqli_real_escape_string($conn,$data["password2"]);
-    
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
 }
