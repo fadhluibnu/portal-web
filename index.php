1a6cb85f701d7f899270f888462a0a298b5c2cc0
fadhluibnu <f.ibnu.a1@gmail.com>
2021-10-27 10:06:14 +0700

    beranda

--- index.php
+++ index.php
@@ -38,7 +38,22 @@
             </div>
         </div>
     </nav>
-
+    <div class="mt-5 pb-5">
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+                <div class="bg-white">
+                    menu
+                </div>
+            </div>
+            <div class="col-8 ">
+                <div class="bg-white">
+                    postingn
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
