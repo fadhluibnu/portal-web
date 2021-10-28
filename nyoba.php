cef9394c86b65f5d11f4b90cf0ba70849282e1a8
fadhluibnu <f.ibnu.a1@gmail.com>
2021-10-28 17:41:53 +0700

    img

--- nyoba.php
+++ nyoba.php
@@ -24,7 +24,11 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["id_user"]. " " . $row["judul_barang"] . $row["deskripsi_barang"] . $row["gambar"]."<br>";
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["id_user"]. " " . $row["judul_barang"] . $row["deskripsi_barang"] ;
+        ?>
+        <img src="img/<?= $row['gambar'] ?>" alt="">
+        <?php
+        echo "<br>"; 
     }
 } else {
     echo "0 results";
