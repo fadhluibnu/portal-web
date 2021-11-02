6ff69bafc0c44087bde47d8941f477b0f667c96b
fadhluibnu <f.ibnu.a1@gmail.com>
2021-11-02 17:53:35 +0700

    detail

--- dashboard/delete.php
+++ dashboard/delete.php
@@ -1,20 +1,17 @@
 <?php
-require 'function.php';
+require '../function.php';
 $id = $_GET["id"];
 
-if (hapus($id) > 0>) {
+if (hapus($id) > 0) {
     echo "
     <script>
     alert('Produk berhasil di hapus');
-    //document.location.href='../dashboard/'
+    document.location.href='../dashboard/'
     </script>";
-}else {
+} else {
     echo "
     <script>
     alert('Produk gagal di hapus');
-    //document.location.href='../dashboard/'
+    document.location.href='../dashboard/'
     </script>";
 }
-
-
-?>
